f3ff53982a0239f7b92d75701a677d908e20cc2b
minsuYun <yms0214@gmail.com>
2019-06-13 00:45:39 +0900

    before testing LeftMessageBubble Styling

--- components/Chat/messageBox.js
+++ components/Chat/messageBox.js
@@ -1,5 +1,8 @@
 import React from "react";
-import MessageBubble from "./messageBubble";
+import { View, StyleSheet, Text } from "react-native";
+import RightMessageBubble from "./rightMessageBubble";
+import LeftMessageBubble from "./leftMessageBubble";
+import { Avatar } from "react-native-elements";
 
 //들어와야 하는 props목록
 //1. 메시지내용(chatMessage)
@@ -9,6 +12,46 @@ import MessageBubble from "./messageBubble";
 //5. 발신인 사진
 
 const MessageBox = ({ text, teamId, teamName, createdAt, img }) => {
-  return <MessageBubble teamId={teamId} text={text} />;
+  if (teamId === 30) {
+    return <RightMessageBubble teamId={teamId} text={text} />;
+  } else {
+    <View style={styles.messageBox}>
+      <View style={styles.teamName}>
+        <Text>{teamName}</Text>
+      </View>
+      <View style={styles.AvatarBubbleContainer}>
+        <View style={styles.avatar}>
+          <Avatar
+            size="small"
+            rounded
+            source={{
+              uri: img
+            }}
+          />
+        </View>
+        <LeftMessageBubble style={styles.bubble} teamId={teamId} text={text} />
+      </View>
+    </View>;
+  }
 };
+
+const styles = StyleSheet.create({
+  messageBox: {
+    flex: 1
+  },
+  teamName: {
+    flex: 0.2
+  },
+  AvatarBubbleContainer: {
+    flex: 0.8,
+    flexDirection: "row"
+  },
+  avatar: {
+    flex: 0.3
+  },
+  bubble: {
+    flex: 0.7
+  }
+});
+
 export default MessageBox;
