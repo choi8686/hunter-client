1f3f24a1baf5b5a162dbf58c115f89688139fa94
iaan <iaan@iaanui-MacBookPro.local>
2019-06-19 15:35:12 +0900

    fixing login Error

--- screens/main/Main.js
+++ screens/main/Main.js
@@ -24,7 +24,7 @@ export default class Main extends Component {
   // 처음 접속시, userToken이 있으면 database에서 나의 해당정보를 가져오고 그 정보를 가지고 DistrictScreen에 접속한다.
   // 데이터 베이스에서 해당정보를 가지고 와야 홍대의 사람들을 보여줄지, 이태원의 사람들을 보여줄지 그리고 또 홍대의 그린라이트를 보여줄지, 한신포차를 보여줄지 알 수 있다.
   _bootstrapAsync = async () => {
-    if (await AsyncStorage.getItem("userToken")) {
+    if ((await AsyncStorage.getItem("userToken")) !== undefined) {
       const userToken = await AsyncStorage.getItem("userToken");
       const userTokenArr = userToken.split("-");
       "aasertetdbc" +
